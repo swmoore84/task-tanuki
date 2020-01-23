c61be8b4dc963e161d8793f1d8f8245dfda6992c
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-23 20:25:47 +0900

    creating first routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new' to: 'tasks#new'
+
+  post 'tasks', to: 'tasks#create'
+
+  get 'dashboard', to: 'pages#dashboard'
+
+  get 'tasks/:id', to: 'tasks#show'
+
+  get 'tasks/:id/edit', to: 'tasks#edit'
+
+  patch 'tasks/:id', to: 'tasks#update'
 end
