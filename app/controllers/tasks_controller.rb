f35df1a7ea341f0c885199b07539ef5b9ab058d4
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-26 14:50:15 +0900

    Created new task form and links on home page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,17 +9,32 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
   end
 
   def create
+    @task = Task.new(task_params)
+    @user = current_user
+    @task.user = @user
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render 'tasks/new'
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :category, :description, :price, :remote_job, :location, :due_date)
   end
 end
