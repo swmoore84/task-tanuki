241d8e5ff13e0a254ce5f7de35013636db336601
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-27 08:40:52 +0900

    added pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  include Pundit
+
+  after_action :verify_authorized, except: %i[index, show], unless: :skip_pundit?
+  # Added "show" to exception policies so that any user can see the task details...
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  # Uncomment when you *really understand* Pundit!
+  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  # def user_not_authorized
+  #   flash[:alert] = "You are not authorized to perform this action."
+  #   redirect_to(root_path)
+  # end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
 end
