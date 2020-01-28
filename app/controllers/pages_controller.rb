c4eeec2a8cc2483e0ce15ecf5ee4b91809852bb7
mohan tupakula <tmskaushik@gmail.com>
2020-01-28 21:33:06 +0900

    added profile photo in edit profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,13 @@
 class PagesController < ApplicationController
   def home
-    # page_params
+    
     @tasks = Task.all
-    @user = current_user
+    
   end
 
   def dashboard
     @tasks = current_user.tasks.all
   end
 
-  # def page_params
-  #   params.require(:user).permit(:photo)
-  # end
+
 end
