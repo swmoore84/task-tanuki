a6a8e45b392f62ec2b10870033b29997de32392c
Sam Moore <swmoore84@gmail.com>
2020-01-28 21:44:28 +0900

    created logic for dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def pending_tasks
+    tasks.joins(:offers).where(offers: { status: "pending" })
+  end
+
+  def pending_tasks?
+    pending_tasks.any?
+  end
+
+  def tasks_without_offers
+    tasks.where('id NOT IN (SELECT DISTINCT(task_id) FROM offers)')
+  end
 end
